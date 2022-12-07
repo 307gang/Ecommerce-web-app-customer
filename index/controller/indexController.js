6f4e7923b8954f1be2607eba3bfd3401968365d6
Le Dang Minh Khoi/ Luigi <khoiledangminh@gmail.com>
2022-12-07 19:33:32 +0700

    update index

--- index/controller/indexController.js
+++ index/controller/indexController.js
@@ -1,47 +1,10 @@
-var productLandingPage = [
-    {
-      product_name: "Áo thun xanh mint",
-      product_image: "/asset/img/ao-thun1.jpg",
-      product_price: "300.000 VNĐ",
-    },
-    {
-      product_name: "Quần jeans đen",
-      product_image: "/asset/img/quan-jean1.jpg",
-      product_price: "350.000 VNĐ",
-    },
-    {
-      product_name: "Váy JK xanh biển",
-      product_image: "/asset/img/vay-jk1.jpg",
-      product_price: "200.000 VNĐ",
-    },
-    {
-      product_name: "Váy Sunflower",
-      product_image: "/asset/img/dam-hoa.jpg",
-      product_price: "950.000 VNĐ",
-    },
-    {
-      product_name: "Quần short xám",
-      product_image: "/asset/img/quan-short1.jpg",
-      product_price: "230.000 VNĐ",
-    },
-    {
-      product_name: "Áo blazer nâu",
-      product_image: "/asset/img/ao-blazer1.jpg",
-      product_price: "500.000 VNĐ",
-    },
-    {
-      product_name: "Áo Vest xanh biển",
-      product_image: "/asset/img/ao-vest1.jpg",
-      product_price: "1.500.000 VNĐ",
-    },
-    {
-      product_name: "Quần kaki xanh đen",
-      product_image: "/asset/img/quan-kaki1.jpg",
-      product_price: "600.000 VNĐ",
-    },
-  ];
+const getProductsList = require('../model/getProductList');
   
-  module.exports = (req, res) => {
-    res.render("", { productLandingPage });
-  };
+module.exports = (req, res) => {
+  (async () => {
+    var result = await getProductsList(req);
+    res.render("index", {products: result.products});
+  })();
+  
+};
   
