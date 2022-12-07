a100b10c00a7b619def33bad7013fe18ae26e624
Teddy <cuongthinh2002@gmail.com>
2022-12-07 18:20:47 +0700

    delete

--- database/route/sameCategoriesRoute.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-var sameCategory = require('../controller/getSameCategoryController');
-
-router.get('/:id', sameCategory);
-
-module.exports = router;
