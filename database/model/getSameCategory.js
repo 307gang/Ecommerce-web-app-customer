81344a19a3372c1dc408a2308b97461fb010a2df
Le Dang Minh Khoi/ Luigi <khoiledangminh@gmail.com>
2022-12-07 19:20:05 +0700

    update related product

--- database/model/getSameCategory.js
+++ database/model/getSameCategory.js
@@ -2,7 +2,7 @@ const db = require("./database");
 
 module.exports = async (id) => {
   const result = await db.connection.execute(
-    "select `cpd.category_id` from `products pd`, `category_product cpd`, `categories c` where `pd.product_id` = `cpd.product_id` and `cpd.category_id` = `c.category_id` and c.category_id = ?",
+    "select pd.* from products pd, category_product cpd1, category_product cpd2 where cpd1.product_id = ? and cpd2.category_id = cpd1.category_id and pd.product_id = cpd2.product_id",
     [id]
   );
   return result[0];
--- product/controller/productController.js
+++ product/controller/productController.js
@@ -1,11 +1,13 @@
 var getProductById = require('../model/getProductById');
+var getRelatedProduct = require('../model/getRelatedProduct')
 const createError = require("http-errors");
 
 module.exports = (req, res, next) => {
     (async () => {
         var {id} = req.params;
-        const result = await getProductById(req, id);
-        if (result.error == 404) next(createError(404));
-        res.render('productDetail', {product: result});
+        const result1 = await getProductById(req, id);
+        const result2 = await getRelatedProduct(req, id);
+        if (result1.error == 404) next(createError(404));
+        res.render('productDetail', {product: result1, related_products: result2.category});
     })();
 }
