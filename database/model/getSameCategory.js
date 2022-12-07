5759eb7796e21c0e5d3537ab0b271adb659523b4
Teddy <cuongthinh2002@gmail.com>
2022-12-07 18:21:08 +0700

    change query and add more routes

--- database/model/getSameCategory.js
+++ database/model/getSameCategory.js
@@ -1,6 +1,9 @@
-const db = require('./database');
+const db = require("./database");
 
 module.exports = async (id) => {
-    const result = await db.connection.execute("select * from categories where id = ?", [id]);
-    return result[0];
-}
+  const result = await db.connection.execute(
+    "select `cpd.category_id` from `products pd`, `category_product cpd`, `categories c` where `pd.product_id` = `cpd.product_id` and `cpd.category_id` = `c.category_id` and c.category_id = ?",
+    [id]
+  );
+  return result[0];
+};
--- database/route/categoriesRoute.js
+++ database/route/categoriesRoute.js
@@ -2,8 +2,9 @@ var express = require("express");
 var router = express.Router();
 
 var all = require('../controller/getCategoryController');  
-
+var sameCategory = require('../controller/getSameCategoryController');
 
 router.get('', all);
+router.get('/:id',  sameCategory);
 
 module.exports = router;
